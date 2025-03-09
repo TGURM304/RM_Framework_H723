47ad438eb6ec9bcfbf24d5dfa8394fd88ce686cc
tiny_fish <i@coraa.cn>
2025-03-09 17:18:18 +0800

    style: reformat code

--- BSP/tim/bsp_tim.h
+++ BSP/tim/bsp_tim.h
@@ -11,14 +11,14 @@
 extern "C" {
 #endif
 
-void bsp_tim_pwm_enable(TIM_HandleTypeDef *h,  uint32_t channel);
+void bsp_tim_pwm_enable(TIM_HandleTypeDef *h, uint32_t channel);
 void bsp_tim_pwm_disable(TIM_HandleTypeDef *h, uint32_t channel);
 void bsp_tim_set(TIM_HandleTypeDef *h, uint32_t period, uint32_t prescaler);
 void bsp_tim_config(TIM_HandleTypeDef *h, double p);
 void bsp_tim_set_blank(TIM_HandleTypeDef *h, uint32_t channel, double blank);
 
-}
 #ifdef __cplusplus
+}
 #endif
 
 #endif //BSP_TIM_H
