44c68bac12ea0243700147a2a972ad023e7976d7
tiny_fish <i@coraa.cn>
2025-03-11 20:24:25 +0800

    feat: 添加 app_msg_can_init 和 app_msg_can_send 用于通过标准 can 收发任意长度数据。

--- APP/msg/app_msg.cc
+++ APP/msg/app_msg.cc
@@ -3,3 +3,71 @@
 //
 
 #include "app_msg.h"
+
+#include "alg_crc.h"
+#include "bsp_can.h"
+#include "sys_queue.h"
+#include "sys_task.h"
+
+#include <algorithm>
+#include <functional>
+#include <utility>
+
+OS::Task can_msg_task_;
+OS::Queue <app_msg_can_t> can_msg_q_(50);
+
+void can_msg_task(void *args) {
+    app_msg_can_t msg {};
+    while(true) {
+        if(can_msg_q_.size()) {
+            can_msg_q_.receive(msg);
+            bsp_can_send(msg.port, msg.id, msg.data.begin());
+        }
+        OS::Task::SleepMilliseconds(1);
+    }
+}
+
+void app_msg_can_send(bsp_can_e e, uint32_t id, uint8_t *s) {
+    std::array <uint8_t, 8> data({});
+    std::copy_n(s, data.size(), data.begin());
+    can_msg_q_.send({
+        .port = e,
+        .id = id,
+        .data = data
+    });
+    if(can_msg_task_.handle_ == nullptr) {
+        can_msg_task_.Create(can_msg_task, static_cast <void *> (nullptr), "msg_can", 512, OS::Task::MEDIUM);
+    }
+}
+
+std::function<void(uint8_t*, uint16_t)> can_recv_callback = nullptr;
+uint8_t can_recv_buf[512], can_recv_sz = 0, can_recv_tot_sz = 0;
+
+void app_msg_can_recv(bsp_can_msg_t *msg) {
+    if(can_recv_sz) {
+        auto len = std::min(can_recv_tot_sz - can_recv_sz, 8);
+        std::copy_n(msg->data, len, can_recv_buf + can_recv_sz);
+        can_recv_sz += len;
+    }
+    else if(msg->data[0] == 0xa5 and msg->data[1] == 0x5a) {
+        can_recv_tot_sz = msg->data[2] + 4; // 帧头 2byte + CRC8 1byte
+        auto len = std::min(can_recv_tot_sz - can_recv_sz, 8);
+        std::copy_n(msg->data, len, can_recv_buf + can_recv_sz);
+        can_recv_sz = std::min(can_recv_tot_sz, static_cast <uint8_t> (8));
+    }
+    if(can_recv_sz == can_recv_tot_sz) {
+        auto crc8 = CRC8::calc(can_recv_buf, can_recv_tot_sz - 1);
+        if(crc8 == can_recv_buf[can_recv_tot_sz - 1]) {
+            if(can_recv_callback != nullptr) {
+                can_recv_callback(can_recv_buf + 3, can_recv_tot_sz - 4);
+            }
+        } else {
+            BSP_ASSERT(0);
+        }
+    }
+}
+
+void app_msg_can_set_callback(bsp_can_e e, uint32_t id, std::function<void(uint8_t*, uint16_t)> callback) {
+    bsp_can_set_callback(e, id, app_msg_can_recv);
+    can_recv_callback = std::move(callback);
+}
--- APP/msg/app_msg.h
+++ APP/msg/app_msg.h
@@ -5,12 +5,17 @@
 #ifndef APP_MSG_H
 #define APP_MSG_H
 
+#include "alg_crc.h"
+#include "bsp_can.h"
+
+
 #include <cstdint>
-#include <initializer_list>
 #include <vector>
-#include <algorithm>
 #include "bsp_uart.h"
 
+#include <algorithm>
+#include <array>
+#include <functional>
 
 /*!
  * 通过 Vofa+ 的 Justfloat 协议发送调试数据（所有数据统一转换为 float）
@@ -18,15 +23,36 @@
  * @param args 待发送的数据，可包含不同类型，但最终都会转换为 float
  * @note app_msg_vofa_send(E_UART_DEBUG, data1, data2, data3);
  */
-template <typename... Args>
-void app_msg_vofa_send(bsp_uart_e e, Args... args) {
+template <typename... Args> void app_msg_vofa_send(bsp_uart_e e, Args... args) {
     union {
         const uint8_t ch[4] = { 0x00, 0x00, 0x80, 0x7f };
         float f;
     } tail;
-    std::vector<float> buffer { static_cast<float>(args)..., tail.f };
-    bsp_uart_send(e, reinterpret_cast<uint8_t*>(buffer.data()), buffer.size() * sizeof(float));
+    std::vector<float> buffer{ static_cast<float>(args)..., tail.f };
+    bsp_uart_send(e, reinterpret_cast<uint8_t *>(buffer.data()), buffer.size() * sizeof(float));
 }
 
+struct app_msg_can_t {
+    bsp_can_e port;
+    uint32_t id;
+    std::array <uint8_t, 8> data;
+};
+
+void app_msg_can_set_callback(bsp_can_e e, uint32_t id, std::function<void(uint8_t*, uint16_t)> callback);
+
+// 原始函数，无 CRC 封包
+void app_msg_can_send(bsp_can_e e, uint32_t id, uint8_t *s);
+
+template <typename T>
+void app_msg_can_send(bsp_can_e e, uint32_t id, T &data) {
+    size_t k = std::ceil((sizeof(T) + 4) / 8.0) * 8;
+    std::vector <uint8_t> pkg(k);
+    pkg[0] = 0xa5, pkg[1] = 0x5a, pkg[2] = sizeof(data);
+    std::copy_n(reinterpret_cast<uint8_t *>(&data), sizeof(T), pkg.begin() + 3);
+    pkg[sizeof(T) + 3] = CRC8::calc(pkg.data(), sizeof(T) + 3);
+    for(size_t i = 0; i < pkg.size(); i += 8) {
+        app_msg_can_send(e, id, pkg.data() + i);
+    }
+}
 
 #endif //APP_MSG_H
